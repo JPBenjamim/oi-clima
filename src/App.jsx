7fb2f05d45939386552fe3f453a32bbb0495e6f6
Jpbenjamim <dev.bejamim@gmail.com>
2022-07-15 11:12:15 -0300

    update api requisition

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,87 @@
 import { useState } from 'react'
 
-
 function App() {
-  const [city, setCity] = useState("")
+  const [city, setCity] = useState('')
+  const [weatherForecast, setweatherForecast] = useState(null);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setCity(e.target.value)
-
   }
 
   const handleSearch = () => {
-    fetch(`http://api.weatherapi.com/v1/current.json?key=5ee6f96a413445b889911145220402&q=${city}&lang=pt`)
-    .then((response) => {
-        console.log
-    })
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=5ee6f96a413445b889911145220402&q=${city}&lang=pt`
+    )
+      .then(response => {
+        if (response.status === 200) {
+          return response.json()
+        }
+      })
+      .then(data => {
+        console.log(data)
+        setweatherForecast(data);
+      })
+
+      
+      
   }
-  
 
   return (
     <div>
-      <nav className='navbar navbar-expand-md navbar-dark  bg-dark mb-4'>
-        <a className='navbar-brand text-whithe' href='#top'> Oi clima</a>
+      
+      <nav className="navbar navbar-expand-md navbar-dark  bg-dark mb-4" >
+      <div className='container d-flex justify-content-center'>
+        
+        <a className="navbar-brand text-whithe" href="#top">
+          {' '}
+          Qual clima? 
+           
+        </a>
+        
+        </div>
       </nav>
+     
 
-      <main className='container'>
-        <div className='bg-light -5'>
-          <h1>
-            Verifique a previsão do tempo da sua cidade!
-          </h1>
-          <p className='lead'>Digite sua cidade e pesquise!</p>
+      <main className="container">
+        <div className="bg-light p-4">
+          <h1>Verifique a previsão do tempo da sua cidade!</h1>
+          <p className="lead">Digite sua cidade e pesquise!</p>
 
-          <div className='row mb-4'>
-            <div className='col-md-6'>
-              <input 
-              onChange={handleChange}
-              className='form-control' value={city}/>
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <input
+                onChange={handleChange}
+                className="form-control"
+                value={city}
+              />
             </div>
           </div>
 
-          <button onClick={handleSearch} className='btn btn-primary btn-lg mb-4'>
+          <button
+            onClick={handleSearch}
+            className="btn btn-primary btn-lg mb-4"
+          >
             Pesquisar
           </button>
+          {weatherForecast ? (
+            <div>
+              <div className="mt-4" d-flex align-itens-center>
+                <div>
+                  <img src={weatherForecast.current.condition.icon} />
+                </div>
+
+                <div>
+                  <h3>
+                    Hoje o dia está {weatherForecast.current.condition.text}
+                  </h3>
+                  <p className="lead">
+                    Temp: {weatherForecast.current.temp_c}º <br /> Humidade {weatherForecast.current.humidity}%
+                  </p>
+                 
+                </div>
+              </div>
+            </div>
+          ) : null}
         </div>
       </main>
     </div>
